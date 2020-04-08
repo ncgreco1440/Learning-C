bb89975a8eaa33ae0097bc538ab786af8363a322
Nico Greco <ncgreco1440@gmail.com>
2020-04-07 23:11:01 -0400

    new functions

--- src/Functions/functions.c
+++ src/Functions/functions.c
@@ -7,12 +7,31 @@ int power(int base, int n) {
   return base;
 }
 
+/**
+ * Exercise 1-15, write up a temperature converter function
+ * Formula
+ * 1) C = (F - 32) * 5/9
+ * 2) F = (C / (5/9)) + 32
+ */
+float toFarenhiet(float celcius) {
+  return (celcius / 0.555555) + 32; 
+}
+
+float toCelcius(float farenhiet) {
+  return (farenhiet - 32) * 0.55555;
+}
+
 int main() {
   int i;
 
   for (i = 0; i < 10; i++) {
     printf("%d %d %d\n", i, power(i, 2), power(-3, i));
   }
+
+  printf("32 degrees Farenheit = %.0f degrees Celcius\n", toCelcius(32));
+  printf("32 degrees Celcius = %.0f degrees Farenheit\n", toFarenhiet(0));
+  printf("212 degrees Farenheit = %.0f degrees Celcius\n", toCelcius(212));
+  printf("100 degrees Celcius = %.0f degrees Farenheit\n", toFarenhiet(100));
 
   return 0;
 }
