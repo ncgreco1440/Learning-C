344be8a1056a9d68a362d3741b94e2091885b1ea
Nico Greco <ncgreco1440@gmail.com>
2020-04-08 21:31:14 -0400

    windows application

---
+++ src/Windows/main.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include <stdlib.h>
+
+int main() {
+  int proc = GetCurrentProcessId();
+  char procStr[10];
+
+  _itoa_s(proc, procStr, 10, 10);
+
+  int msgId = MessageBox(NULL, procStr, "Hello, World", MB_OK);
+  if (msgId == IDOK) {
+    MessageBox(NULL, "Congratz, you clicked 'OK'", "Congratz", MB_OK);
+  }
+  return 0;
+}
